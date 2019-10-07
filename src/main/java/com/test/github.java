49225af91e012928f0a5079936f95346c409b139
itcast <hello@itcast.cn>
2019-10-07 21:24:32 +0800

    二次提交代码
    Signed-off-by: Li <hello@itcast.cn>

--- src/main/java/com/test/github.java
+++ src/main/java/com/test/github.java
@@ -3,5 +3,6 @@ package com.test;
 public class github {
     public static void main(String[] args) {
         System.out.println("github首次提交代码");
+        System.out.println("github二次提交代码");
     }
 }
